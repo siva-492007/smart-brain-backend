93fabac94a08a0e9bd1db7eccfdf3c0e55133c10
siva-007 <sivavignesh761@gmail,com>
2021-07-06 19:33:22 +0530

    backend congif 2

--- controllers/image.js
+++ controllers/image.js
@@ -15,7 +15,8 @@ const handleApiCall = (req, res) => {
     // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
     // to:
     // .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .then(data => {
       res.json(data);
     })
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res)=> { res.send('working') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.put('/update-image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
